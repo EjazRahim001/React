11181ecf281c4cecb5a72869acc1ff9be34a3d50
EjazRahim001 <ejaz.rahim125@gmail.com>
2025-11-09 03:30:32 +0500

    Welcome to Event Planner

---
+++ event_planner/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import EventPlanner from './Components/EventPlanner'
+import Footer from './Components/Footer'
+
+function App() {
+  return (
+ <>
+ <EventPlanner/>
+ <Footer />
+ </>
+  )
+}
+
+export default App
